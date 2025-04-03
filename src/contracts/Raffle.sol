0b1ac420457182c8e9e2e65b05f64986319941bd
elian <elianmars13@gmail.com>
2025-04-03 08:29:05 +0300

    first commit

---
+++ src/contracts/Raffle.sol
@@ -0,0 +1,55 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+/**
+ * @title A sample Raffle Contract
+ * @author Elian
+ * @notice This contract is for creating a sample raffle
+ * @dev It implements Chainlink VRFv2.5 and Chainlink Automation
+ */
+
+contract Raffle {
+    /* errors*/
+    error Raffle__NotEnoughEthSent();
+
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+    function enterRaffle() public payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent!");
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEthSent();
+            s_players.push(payable(msg.sender));
+        }
+    }
+
+    function pickWinner() public {}
+    /** Getter Function */
+
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+}
