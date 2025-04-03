ba0cd8bd87063104c853b7db6095e5e5d71bdc46
elian <elianmars13@gmail.com>
2025-04-03 09:55:43 +0300

    events

--- src/contracts/Raffle.sol
+++ src/contracts/Raffle.sol
@@ -34,6 +34,8 @@ contract Raffle {
 
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    /* Events */
+    event RaffleEntered(address indexed player);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
@@ -43,8 +45,9 @@ contract Raffle {
         // require(msg.value >= i_entranceFee, "Not enough ETH sent!");
         if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughEthSent();
-            s_players.push(payable(msg.sender));
         }
+        s_players.push(payable(msg.sender));
+        emit RaffleEntered(msg.sender);
     }
 
     function pickWinner() public {}
